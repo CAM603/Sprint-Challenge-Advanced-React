d2db2af22834058facc259f19dc76aa2d770af39
CAM603 <cameronhawley@comcast.net>
2020-01-17 14:39:21 -0500

    added more tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
-import ReactDOM from 'react-dom';
+
 import App from './App';
 
 afterEach(cleanup);
@@ -18,3 +18,74 @@ test('Toggle button changes text', () => {
   expect(toggleBtn.textContent).toBe('Light Mode');
   
 });
+
+describe('All countries are rendered', () => {
+  test('United States is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/united states/i)
+  })
+  
+  test('Brazil is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/brazil/i)
+  })
+  
+  test('France is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/france/i)
+  })
+
+  test('Sweden is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/sweden/i)
+  })
+  
+  test('Netherlands is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/netherlands/i)
+  })
+  test('Australia is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/australia/i)
+  })
+  
+  test('Chile is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/chile/i)
+  })
+  
+  test('England is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/england/i)
+  })
+
+  test('Italy is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/italy/i)
+  })
+  
+  test('Germany is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/germany/i)
+  })
+
+  test('Canada is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/canada/i)
+  })
+  
+  test('Spain is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/spain/i)
+  })
+
+  test('Norway is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/norway/i)
+  })
+  
+  test('Argentina is rendered', () => {
+    const {getByText} = render(<App />);
+    getByText(/argentina/i)
+  })
+})
